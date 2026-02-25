c009f0620b858a9d37dcc047a779c175ff7d5082
Abdellah-Moukarrame <moukarrameabdellah4@gmail.com>
2026-02-25 10:30:12 +0100

    add an admin to the plateform by seeders

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('users')->insert(
+            [
+                'name' => 'Global Admin',
+                'email' => 'admin@admin.com',
+                'password' => bcrypt('admin@admin.com'),
+                'role' => 'admin',
+                'reputation'=>0,
+                'is_banned'=>false
+            ]
+        );
+    }
+}
