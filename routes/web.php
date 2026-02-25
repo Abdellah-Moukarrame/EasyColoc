989fc07318e6f14f769c8a8989e76ee96259bab3
Abdellah-Moukarrame <moukarrameabdellah4@gmail.com>
2026-02-25 10:31:48 +0100

    add some new routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,24 @@
 <?php
 
+use App\Http\Controllers\Admin\DashboardController;
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\WelcomeDashboardController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/',[HomeController::class,'index'])->name('home');
+Route::get('/register',[RegisterController::class,'index'])->name('register');
+Route::post('/register',[RegisterController::class,'register'])->name('register.create');
+Route::get('/login',[LoginController::class,'index'])->name('login');
+Route::post('/login',[LoginController::class,'login'])->name('login.create');
+
+
+
+/////admin routing
+
+Route::get('/admin',[DashboardController::class,'index'])->name('admin.dashboard');
+
+//// welcoming dashboard
+
+Route::get('/welcome',[WelcomeDashboardController::class,'index'])->name('welcome.dashboard');
