e59e20067257bb400a16cd954680b6ec61462edf
Abdellah-Moukarrame <moukarrameabdellah4@gmail.com>
2026-02-27 00:22:04 +0100

    new routes

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,14 @@
 use App\Http\Controllers\Admin\DashboardController;
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\Category\CategoryController;
+use App\Http\Controllers\Colocation\ColocationController;
+use App\Http\Controllers\Depences\DepencesController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\Invitation\InvitationController;
+use App\Http\Controllers\Mail\MailController;
 use App\Http\Controllers\WelcomeDashboardController;
+use App\Http\Middleware\AdminMiddleware;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/',[HomeController::class,'index'])->name('home');
@@ -17,8 +23,22 @@ Route::post('/login',[LoginController::class,'login'])->name('login.create');
 
 /////admin routing
 
-Route::get('/admin',[DashboardController::class,'index'])->name('admin.dashboard');
+Route::get('/admin',[DashboardController::class,'index'])->name('admin.dashboard')->middleware(AdminMiddleware::class);
 
 //// welcoming dashboard
 
 Route::get('/welcome',[WelcomeDashboardController::class,'index'])->name('welcome.dashboard');
+Route::post('/welcome',[ColocationController::class,'create'])->name('welcome.create_colocation');
+
+
+//// colocations
+
+Route::get('/colocation/{id}',[ColocationController::class,'index'])->name('colocation.show');
+////  category
+Route::post('/category/add',[CategoryController::class,'creat'])->name('category');
+Route::post('/invitation/add',[InvitationController::class,'create'])->name('invitation.add');
+Route::post('/despence/add',[DepencesController::class,'create'])->name('depences.add');
+Route::delete('/category/delete/{id}', [CategoryController::class, 'destroy'])->name('category.destroy');
+
+//// Invitation
+Route::get('/send_invitation',[MailController::class,"index"])->name('send_invitation');
