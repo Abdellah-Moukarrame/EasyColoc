ccef70d5d073c4e38b6c48e6782a0466d5f0b69c
Abdellah-Moukarrame <moukarrameabdellah4@gmail.com>
2026-03-05 12:08:40 +0100

    version finale

--- routes/web.php
+++ routes/web.php
@@ -15,50 +15,46 @@ use App\Http\Middleware\AdminMiddleware;
 use App\Http\Middleware\ColocationMiddleware;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/',[HomeController::class,'index'])->name('home');
-Route::get('/register',[RegisterController::class,'index'])->name('register');
-Route::post('/register',[RegisterController::class,'register'])->name('register.create');
-Route::get('/login',[LoginController::class,'index'])->name('login');
-Route::post('/login',[LoginController::class,'login'])->name('login.create');
-Route::post('/logout',[LoginController::class,'logout'])->name('logout')->middleware('auth');
+Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::get('/register', [RegisterController::class, 'index'])->name('register')->middleware('guest');
+Route::post('/register', [RegisterController::class, 'register'])->name('register.create')->middleware('guest');
+Route::get('/login', [LoginController::class, 'index'])->name('login')->middleware('guest');
+Route::post('/login', [LoginController::class, 'login'])->name('login.create')->middleware('guest');
+Route::post('/logout', [LoginController::class, 'logout'])->name('logout')->middleware('auth');
 
+///// admin routing
+Route::prefix('admin')->middleware(AdminMiddleware::class)->group(function () {
+    Route::get('/', [DashboardController::class, 'index'])->name('admin.dashboard');
+    Route::post('/banUser/{id}', [DashboardController::class, 'ban'])->name('user.ban');
+    Route::post('/unbanUser/{id}', [DashboardController::class, 'unban'])->name('user.unban');
+});
 
+//// authenticated routes
+Route::middleware('auth')->group(function () {
 
-/////admin routing
+    // welcome dashboard
+    Route::get('/welcome', [WelcomeDashboardController::class, 'index'])->name('welcome.dashboard');
+    Route::post('/welcome', [ColocationController::class, 'create'])->name('welcome.create_colocation');
 
-Route::get('/admin',[DashboardController::class,'index'])->name('admin.dashboard')->middleware(AdminMiddleware::class);
+    // colocations
+    Route::get('/colocation/{id}', [ColocationController::class, 'index'])->name('colocation.show')->middleware(ColocationMiddleware::class);
 
-//// welcoming dashboard
+    // categories
+    Route::post('/category/add', [CategoryController::class, 'creat'])->name('category');
+    Route::delete('/category/delete/{id}', [CategoryController::class, 'destroy'])->name('category.destroy');
 
-Route::get('/welcome',[WelcomeDashboardController::class,'index'])->name('welcome.dashboard');
-Route::post('/welcome',[ColocationController::class,'create'])->name('welcome.create_colocation');
+    // invitations
+    Route::post('/invitation/add', [InvitationController::class, 'create'])->name('invitation.add');
+    Route::post('/send_invitation', [MailController::class, 'index'])->name('send_invitation');
+    Route::get('/accept_invitation', [InvitationController::class, 'accept'])->name('acceptInvit');
 
+    // depences
+    Route::post('/despence/add', [DepencesController::class, 'create'])->name('depences.add');
+    Route::get('/depences/{id}', [DepencesController::class, 'index'])->name('depences.index')->middleware(ColocationMiddleware::class);
 
-//// colocations
+    // statements
+    Route::patch('/statement/{id}/paid', [DepencesController::class, 'markPaid'])->name('statement.paid');
 
-Route::get('/colocation/{id}',[ColocationController::class,'index'])->name('colocation.show')->middleware(['auth',ColocationMiddleware::class]);
-////  category
-Route::post('/category/add',[CategoryController::class,'creat'])->name('category');
-Route::post('/invitation/add',[InvitationController::class,'create'])->name('invitation.add');
-Route::post('/despence/add',[DepencesController::class,'create'])->name('depences.add');
-Route::delete('/category/delete/{id}', [CategoryController::class, 'destroy'])->name('category.destroy');
-
-//// Invitation
-Route::post('/send_invitation',[MailController::class,"index"])->name('send_invitation');
-Route::get('/accept_invitation',[InvitationController::class,'accept'])->name('acceptInvit');
-
-
-////Ban & unban Users
-
-Route::post('/admin/banUser/{id}',[DashboardController::class,'ban'])->name('user.ban');
-Route::post('/admin/unbanUser/{id}',[DashboardController::class,'unban'])->name('user.unban');
-
-/// depences
-Route::get('/depences/{id}', [DepencesController::class, 'index'])->name('depences.index')->middleware(['auth', ColocationMiddleware::class]);
-
-/// statements
-
-Route::patch('/statement/{id}/paid', [DepencesController::class, 'markPaid'])->name('statement.paid');
-
-
-Route::post('/membership/leave', [MembershipController::class, 'quitter'])->name('membership.quitter')->middleware('auth');
+    // membership
+    Route::post('/membership/leave', [MembershipController::class, 'quitter'])->name('membership.quitter');
+});
